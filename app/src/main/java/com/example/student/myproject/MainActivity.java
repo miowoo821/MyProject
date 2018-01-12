981ee642b1d155bb927d6a8fb91f771c19922432
miowoo <miowoo821@gmail.com>
2018-01-12 16:04:04 +0800

    20180112主頁介面

--- app/src/main/java/com/example/student/myproject/MainActivity.java
+++ app/src/main/java/com/example/student/myproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.student.myproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.newconsumption,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //點選menu(+)後要觸發下面程式碼
+
+        return super.onOptionsItemSelected(item);
     }
 }
