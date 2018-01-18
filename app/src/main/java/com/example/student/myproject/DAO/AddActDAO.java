727691f65ac7b980c5e7f918926ef02bfa3d42ef
miowoo821 <miowoo821@gmail.com>
2018-01-18 21:09:25 +0800

    初試DAO

--- app/src/main/java/com/example/student/myproject/DAO/AddActDAO.java
+++ app/src/main/java/com/example/student/myproject/DAO/AddActDAO.java
@@ -1,6 +1,7 @@
 package com.example.student.myproject.DAO;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,56 +10,110 @@ import android.widget.TextView;
 
 import com.example.student.myproject.ActivityPage;
 import com.example.student.myproject.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Student on 2018/1/17.
  */
-
+//僅在RAM裡面的新增查詢修改刪除
 public class AddActDAO {
-    LayoutInflater inflater=LayoutInflater.from(ActivityPage.this);
-    final View newactivity=inflater.inflate(R.layout.newactivity,null);
-
-
-    EditText act_name=newactivity.findViewById(R.id.act_name);
-    EditText limted=newactivity.findViewById(R.id.limted);
-    TextView act_S_D=newactivity.findViewById(R.id.act_start_date);
-    TextView act_E_D=newactivity.findViewById(R.id.act_end_date);
-    TextView F_S_D=newactivity.findViewById(R.id.feedback_start_date);
-    TextView F_E_D=newactivity.findViewById(R.id.feedback_end_date);
-    EditText ratio=newactivity.findViewById(R.id.ratio);
-    EditText memo=newactivity.findViewById(R.id.memo);
-    ContentValues cv=new ContentValues();
-                cv.put("act_name",act_name.toString());
-                cv.put("limted",limted.toString());
-                cv.put("act_S_D",act_S_D.toString());
-                cv.put("act_E_D",act_E_D.toString());
-                cv.put("F_S_D",F_S_D.toString());
-                cv.put("F_E_D",F_E_D.toString());
-                cv.put("ratio",ratio.toString());
-                cv.put("memo",memo.toString());
-                dbW.insert("act_list",null,cv);
-
-    String _id="序號,";
-    String name="活動名稱,";
-    String lim="回饋上限,";
-    String S_D="活動開始日期,";
-    String[] colum={"_id","act_name","limted","act_S_D","act_E_D","F_S_D","F_E_D","ratio","memo"};
-    Cursor c;
-    c=dbR.query("act_list",colum,null,null,null,null,null);
-
-
-
-                if (c.getCount()>0){
-        c.moveToFirst();
-        for(int X=0 ; X<c.getCount();X++){
-//                        _id +=(X+1);
-//                      name+=c.getString(0) ;
-//                        lim+= c.getString(1)+"\n";
-            String A=c.getString(0);
-            String  B=c.getString(1);
-            String C=c.getString(2);
-            String  D=c.getString(3);
-            c.moveToNext();
+    public ArrayList<act> act_list;
+    Context context;//存在SQLite還需要取得可儲存檔案路徑嗎????
+    public AddActDAO(Context context){
+        this.context=context;
+        act_list = new ArrayList<>();
+    }
+//    public AddActDAO(){
+//        this.act_list=new ArrayList<>();
+//    }
+    public void saveFile(){
+        File f=new File(context.getFilesDir(),"act_list");
+        FileWriter fw=null;
+        try {
+            fw=new FileWriter(f);
+            Gson gson = new Gson();
+            String act_data=gson.toJson(act_list);
+            fw.write(act_data);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+    public  void loadFile(){
+        File f=new File(context.getFilesDir(),"act_list");
+        FileReader fr=null;
+        try {
+            fr=new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String str = br.readLine();
+            Gson gson = new Gson();
+            act_list=gson.fromJson(str,new TypeToken<ArrayList<act>>(){}.getType());
+            br.close();//寫完關閉
+            fr.close();//寫完關閉
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean act_add(act Act){//因為act.java的名稱打小寫了，為了區分，變數名稱就打大寫了(跟一般慣例相反)
+
+        act_list.add(Act);
+        saveFile();
+     return true;
+    }
+    public ArrayList<act> getList(){
+        loadFile();
+        return act_list;
+    }
+
+    public act get_Act(String id){
+        for(act A:act_list){
+            loadFile();
+            if (A._ID==id){
+                return A;
+            }
+        }
+        return  null;
+    }
+    public boolean update(act Act){
+        for(act G:act_list){
+            loadFile();
+            if (G._ID==Act._ID){
+                G.Act_Name=Act.Act_Name;
+                G.FeedBack_Point_Limited=Act.FeedBack_Point_Limited;
+                G.FeedBack_Ratio=Act.FeedBack_Ratio;
+                G.Act_S_Date=Act.Act_S_Date;
+                G.Act_E_Date=Act.Act_E_Date;
+                G.FeedBack_S_Date=Act.FeedBack_S_Date;
+                G.FeedBack_E_Date=Act.FeedBack_E_Date;
+                G.Memo=Act.Memo;
+                saveFile();
+                return true;
+            }
+        }
+        return  false;
+    }
+    public boolean delete(String ID){
+        for(int i=0;i<act_list.size();i++){
+            if (act_list.get(i)._ID==ID){//搜尋資料庫裡面與剛輸入的ID相符合的資料
+
+                act_list.remove(i);
+                saveFile();
+                return true;
+            }
+        }
+        return  false;
+    }
+
 }
--- app/src/main/java/com/example/student/myproject/DAO/act.java
+++ app/src/main/java/com/example/student/myproject/DAO/act.java
@@ -5,17 +5,33 @@ import android.widget.TextView;
 
 import com.example.student.myproject.R;
 
+import java.util.Date;
+
 /**
  * Created by Student on 2018/1/17.
  */
 
 public class act {
-   View newactivity;
-    final TextView tv1=newactivity.findViewById(R.id.act_start_date);
-    final TextView tv2=newactivity.findViewById(R.id.act_end_date);
-    final TextView tv3=newactivity.findViewById(R.id.feedback_start_date);
-    final TextView tv4=newactivity.findViewById(R.id.feedback_end_date);
+    String _ID;//ID不能拿來做加減的
+    public String Act_Name;
+    public int FeedBack_Point_Limited;
+    public int FeedBack_Ratio;
+    public  Date Act_S_Date;
+    public Date Act_E_Date;
+    public   Date FeedBack_S_Date;
+    public  Date FeedBack_E_Date;
+    public  String Memo;
 
+    public act(String Act_Name,int FeedBack_Point_Limited,int FeedBack_Ratio,Date Act_S_Date,Date Act_E_Date,Date FeedBack_S_Date,  Date FeedBack_E_Date,String Memo){
 
+        this.Act_Name=Act_Name;
+        this.FeedBack_Point_Limited=FeedBack_Point_Limited;
+        this.FeedBack_Ratio=FeedBack_Ratio;
+        this.Act_S_Date=Act_S_Date;
+        this.Act_E_Date=Act_E_Date;
+        this.FeedBack_S_Date=FeedBack_S_Date;
+        this.FeedBack_E_Date=FeedBack_E_Date;
+        this.Memo=Memo;
+    }
 
 }
